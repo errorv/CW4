ea5992ee98d19941e8780de02190402b14c15412
Jakoob <Jakoob@DESKTOP-NFAM58N>
2017-04-03 21:34:50 +0200

    Commit

---
+++ CW4.1/CW4.1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CW4._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Uczelnia UWM = new Uczelnia(DateTime.Parse("2099/01/01"));
+            Uczelnia Stamford = new Uczelnia(DateTime.Parse("2100/01/01"));
+
+            Console.WriteLine(UWM.RekrutujStudentow());
+            Console.WriteLine(Stamford.RekrutujStudentow());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CW4.1/CW4.1/Uczelnia.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CW4._1
+{
+    class Uczelnia
+    {
+        private static Uczelnia _Instance;
+        private DateTime _dataRekrutacji;
+        public Uczelnia(DateTime datarekrutacji)
+        {
+            _dataRekrutacji = datarekrutacji;
+        }
+        public static Uczelnia Instance(DateTime dataRekrutacji)
+        {
+            if (_Instance == null)
+            {
+                _Instance = new Uczelnia(dataRekrutacji);
+
+                return _Instance;
+            }
+            return _Instance;
+
+        }
+        public DateTime RekrutujStudentow()
+        {
+            return _dataRekrutacji;
+        }
+    }
+}
